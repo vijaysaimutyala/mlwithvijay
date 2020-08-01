ab1473683a0610bc6adace0fea4d9184023f4477
Vijay Sai Mutyala <vijsuvig@gmail.com>
2020-08-01 22:44:15 +0530

    setup.sh correction

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
-echo "\
-[general]\n\
-email = \"vijaysai.mutyala@gmail.com\"\n\
-" > ~.streamlit/credentials.toml
+mkdir -p ~/.streamlit
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
